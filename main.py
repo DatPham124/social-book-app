091b4b0256895686b9c6445ffd1ca9a0ed3ebba1
DatPham124 <ppap1242003@gmail.com>
2025-09-16 14:44:45 +0700

    fix(user_service): change engine database

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from .routes import users, admin, profiles
-from common_lib.database import engine_book_service
+from common_lib.database import engine_user_srevice
 from sqlmodel import SQLModel, Session, select
 from .model import Role
 
@@ -9,8 +9,8 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
 
-    SQLModel.metadata.create_all(engine_book_service)
-    with Session(engine_book_service) as session:
+    SQLModel.metadata.create_all(engine_user_srevice)
+    with Session(engine_user_srevice) as session:
         existing_roles = session.exec(select(Role)).all()
         if not existing_roles:
             roles = [
